3184458a5898cb5dfaa3dcf5abcdefb6ba446bf9
mazhichao <48847514+caterpillarmzc@users.noreply.github.com>
2019-03-31 22:25:21 +0800

    Add files via upload

---
+++ 练习9/练习9/test.c
@@ -0,0 +1,42 @@
+﻿//#include<stdio.h>
+////宏定义&&为and
+//#define and && 
+//int main(){
+//	//unsigned int x = 0 * 123;
+//	//unsigned int y = 0;
+//	////整数之间的转换
+//	////长度低的网长度高的变量转,高位补符号位
+//	////如果搞得往长度低的变量转,此时发生截断,只保留符号位
+//	//y = x;
+//	////整数和浮点数内存表示数据的方式是不同的
+//	////并不是简单地截断和补位
+//	////最终的目标让转换的结果尽可能正确
+//	//printf("%x\n",y);
+//
+//
+//	system("pause");
+//	return 0;
+//}
+#include <stdio.h>
+//int main() {
+//	int i = 0, a = 0, b = 2, c = 3, d = 4; 
+//	//后置++返回之前的值,a为0,假值
+//	i = a++ && ++b && d++;
+//	//i = a++||++b||d++;  
+//	//对于逻辑与来说,左侧表达式的值为0,右侧不再计算
+//	//或运算,左侧值为非0,右侧不再计算
+//	printf("a = %d\n b = %d\n c = %d\nd = %d\n", a, b, c, d); 
+//	return 0; } 
+//int Max(int x, int y){
+//	return x > y ? x : y + 100;
+//}
+int main(){
+	//int i = 1;
+	//int ret = ++i + ++i + ++i;//未定义行为,对i的修改顺序不确定
+	//printf("ret=%d\n",ret);
+
+
+
+	system("pause");
+	return 0;
+}
