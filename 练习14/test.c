2ec1e25675f5cef09f5c354becba6bea39e95f05
mazhichao <48847514+caterpillarmzc@users.noreply.github.com>
2019-04-14 15:03:12 +0800

    Add files via upload

---
+++ 练习14/test.c
@@ -0,0 +1,29 @@
+//#include <stdio.h>
+//void Print(int x){
+//	if (x>9)
+//	{
+//		Print(x / 10);
+//	}
+//	printf(" %d", x % 10);
+//}
+//
+//int main(){
+//	Print(1234);
+//	system("pause");
+//}
+//递归实现求字符串的长度(不允许创建临时变量)
+//int Strlen(char str[]){
+//	if (str[0] == '\0'){
+//		return 0;
+//	}
+//	return 1 + Strlen(str + 1);
+//}
+//
+//int main(){
+//	char str[] = "abcdefghijklmnopqrstuvwxyz";
+//	int ret = Strlen(str);
+//	printf("ret=%d\n", ret);
+//	system("pause");
+//	return 0;
+//}
+
